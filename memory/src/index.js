9ee6283cb72bb4002ee920a2be14b4fa9cfed247
moueza <>
2020-12-24 12:18:45 +0100

    Resolves #31 p2c2part4par4 Écrire le composant Application from Correction fin; see #1 general eval OK nice emojis

--- memory/src/index.js
+++ memory/src/index.js
@@ -7,18 +7,9 @@ import Card from './Card';
 /*https://openclassrooms.com/fr/courses/4664381-realisez-une-application-web-avec-react-js/4664871-mettez-en-place-un-etat-local*/
 //const HIDDEN_SYMBOL = "?";
 /*Already from Card.js*/
+ 
 
-const Card = ({ card, feedback }) => (
-  <div className={`card ${feedback}`}>
-    <span className="symbol">
-      {feedback === "hidden" ? HIDDEN_SYMBOL : card}
-    </span>
-  </div>
-);
-
-const GuessCount = ({ guesses }) => <div className="guesses">{guesses}</div>
-
-ReactDOM.render(<Card />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
